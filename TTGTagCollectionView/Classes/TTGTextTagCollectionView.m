58ec20581ddd82020245f0d16859a746c3e724d1
Jordan S <jschaenzle@gmail.com>
2018-10-23 16:45:07 -0400

    Allow User Interaction on tag's label
    UI tests are unable to tap the tag views via the accessibility identifier because the TTGTextTagLabel does not have an identifier - the gradient label within it does. However, the gradient label is currently not tappable. This change makes the label tappable so that UI tests can find the TTGTextTagLabel via the gradient label and tap it.

--- TTGTagCollectionView/Classes/TTGTextTagCollectionView.m
+++ TTGTagCollectionView/Classes/TTGTextTagCollectionView.m
@@ -662,6 +662,7 @@
 - (TTGTextTagLabel *)newLabelForTagText:(NSString *)tagText withConfig:(TTGTextTagConfig *)config {
     TTGTextTagLabel *label = [TTGTextTagLabel new];
     label.label.text = tagText;
+    label.label.userInteractionEnabled = YES;
     label.config = config;
     [self updateStyleAndFrameForLabel:label];
     return label;
